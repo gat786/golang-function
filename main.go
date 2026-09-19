10c29618b3b191aa627d5dfdcb21569f1b4abdc2
Ganesh Tiwari <ganesht049@gmail.com>
2026-09-19 21:02:04 +0530

    fix: go thing
    Signed-off-by: Ganesh Tiwari <ganesht049@gmail.com>

--- main.go
+++ main.go
@@ -1,31 +1,22 @@
-package helloworld
+package myhttpfunction
 
 import (
-  "encoding/json"
-  "fmt"
-  "html"
-  "net/http"
+    "fmt"
+    "net/http"
 
-  "github.com/GoogleCloudPlatform/functions-framework-go/functions"
+    "github.com/GoogleCloudPlatform/functions-framework-go/functions"
 )
 
 func init() {
-   functions.HTTP("HelloHTTP", helloHTTP)
+    // Register an HTTP function with the Functions Framework
+    functions.HTTP("MyHTTPFunction", myHTTPFunction)
 }
 
-// helloHTTP is an HTTP Cloud Function with a request parameter.
-func helloHTTP(w http.ResponseWriter, r *http.Request) {
-  var d struct {
-    Name string `json:"name"`
-  }
-  if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
-    fmt.Fprint(w, "Hello, World!")
-    return
-  }
-  if d.Name == "" {
-    fmt.Fprint(w, "Hello, World!")
-    return
-  }
-  fmt.Fprintf(w, "Hello, %s!", html.EscapeString(d.Name))
+// Function myHTTPFunction is an HTTP handler
+func myHTTPFunction(w http.ResponseWriter, r *http.Request) {
+    // Your code here
+
+    // Send an HTTP response
+    fmt.Fprintln(w, "OK")
 }
 
